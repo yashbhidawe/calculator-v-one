7129ae0f40a75ed04919ccb934e33bcd73753abe
Yash Bhidawe <103628487+yashbhidawe@users.noreply.github.com>
2022-10-22 12:23:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+let num1 = 69
+let num2 = 69
+let sum
+document.getElementById("num1-el").textContent = num1
+document.getElementById("num2-el").textContent = num2
+// document.getElementById("sum-el").textContent = sum
+
+// Create four functions: add(), subtract(), divide(), multiply()
+function add() {
+    console.log(sum_el.textContent = num1 + num2)
+    // sum = num1 + num2 
+ 
+}
+
+function subtract() {
+    //  sum = num1 + num2 
+  console.log(sum_el.textContent = num1 - num2)
+ 
+}
+
+function divide() {
+      console.log(sum_el.textContent = num1 / num2)
+    //  sum = num1 + num2 
+}
+function multiply() {
+  console.log(sum_el.textContent = num1 * num2)   
+
+}
+
+
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+
+
